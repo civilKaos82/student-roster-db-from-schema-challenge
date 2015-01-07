a40a3fa5b901bd11bc44ecacf5b5f765b1c7a66f
Torey Hickman <torey@toreyhickman.com>
2015-01-07 09:00:21 -0600

    Refactor repetitive before(:each) from tests

--- source/student_spec.rb
+++ source/student_spec.rb
@@ -20,6 +20,12 @@ describe Student do
       "phone"      => "630-363-6640" }
   end
 
+  let(:student) do
+    mikee = Student.new(mikee_data)
+    mikee.save
+    mikee
+  end
+
   let(:mikee_2_data) do
     { "first_name" => "Mikee",
       "last_name"  => "Baker",
@@ -29,28 +35,27 @@ describe Student do
       "phone"      => "503-333-7740" }
   end
 
+  let(:other_student) do
+    student = Student.new(mikee_2_data)
+    student.save
+    student
+  end
+
 
   describe "#save" do
     context "record not in the database" do
-
-      let(:student) { Student.new(mikee_data) }
+      let(:unsaved_student) { Student.new(mikee_data) }
 
       it "saves to the database" do
-        expect { student.save }.to change { $db.execute("SELECT * FROM students WHERE first_name = ?", 'Mikee').count }.from(0).to(1)
+        expect { unsaved_student.save }.to change { $db.execute("SELECT * FROM students WHERE first_name = ?", 'Mikee').count }.from(0).to(1)
       end
 
       it "assign the id created by the database to the object" do
-        expect { student.save }.to change { student.id }.from(nil).to( $db.last_insert_row_id )
+        expect { unsaved_student.save }.to change { unsaved_student.id }.from(nil).to( $db.last_insert_row_id )
       end
     end
 
     context "record exists in the database" do
-      let(:student) do
-        mikee = Student.new(mikee_data)
-        mikee.save
-        mikee
-      end
-
       it "updates the database columns with the attributes of the object" do
         # Change the first_name attribute in the Ruby object
         student.first_name = "Michael"
@@ -61,40 +66,27 @@ describe Student do
   end
 
   describe ".find" do
-    before :each do
-      @student = Student.new(mikee_data)
-      @student.save
-    end
-
     it "allows me to find a unique student" do
-      found_student = Student.find(@student.id)
+      found_student = Student.find(student.id)
       expect(found_student).to be_a Student
       expect(found_student.first_name).to eq("Mikee")
     end
   end
 
   describe "#delete" do
-    before :each do
-      @student = Student.new(mikee_data)
-      @student.save
-    end
-
     it "deletes a student" do
-      student = Student.find(@student.id)
-      expect(student).to be_a Student
+      found_student = Student.find(student.id)
+      expect(found_student).to be_a Student
 
-      student.delete
-      expect(Student.find(@student.id)).to be(nil)
+      found_student.delete
+      expect(Student.find(student.id)).to be(nil)
     end
   end
 
   describe ".all" do
-    before :each do
-      @student = Student.new(mikee_data)
-      @student.save
-    end
-
     it "retrieves all students at once" do
+      saved_students = [student, other_student]
+
       student_count = $db.execute("SELECT * FROM students").length
       expect(Student.all.size).to eq(student_count)
       expect(Student.all.first).to be_a(Student)
@@ -103,40 +95,30 @@ describe Student do
   end
 
   describe ".find_by_first_name" do
-    before :each do
-      Student.new(mikee_data).save
-      Student.new(mikee_2_data).save
-    end
-
     it "retrieves students based on first name" do
-      students = Student.find_by_first_name("Mikee")
-      expect(students.size).to eq(2)
-      expect(students.first).to be_a(Student)
-      expect(students[1].last_name).to eq("Baker")
+      expected_students = [student, other_student]
+      found_students = Student.find_by_first_name("Mikee")
+
+      expect(found_students.size).to eq expected_students.size
+      expect(found_students.first).to be_a Student
+      expect(found_students[1].last_name).to eq "Baker"
     end
   end
 
   describe ".where" do
-    before :each do
-      Student.new(mikee_data).save
-    end
-
     it "retrieves students by any attribute" do
-      students = Student.where("last_name = ?", "Pourhadi")
+      students = Student.where("last_name = ?", student.last_name)
       expect(students.size).to eq(1)
       expect(students.first).to be_a(Student)
     end
   end
 
   describe ".all_by_birthday" do
-    before :each do
-      Student.new(mikee_data).save
-      Student.new(mikee_2_data).save
-    end
-
     it "orders students by birthday oldest to youngest" do
-      expect(Student.all_by_birthday[0].last_name).to eq("Pourhadi")
-      expect(Student.all_by_birthday[1].last_name).to eq("Baker")
+      students_ordered_by_birthday = [student, other_student].sort_by(&:birthday)
+
+      expect(Student.all_by_birthday[0].last_name).to eq students_ordered_by_birthday[0].last_name
+      expect(Student.all_by_birthday[1].last_name).to eq students_ordered_by_birthday[1].last_name
     end
   end
 end
